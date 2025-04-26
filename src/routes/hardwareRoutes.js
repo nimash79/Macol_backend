fdcd918a4f296982539caa86e7848df7cafead2b
Nima <nimash7979@gmail.com>
2025-04-26 08:42:40 +0330

    refactor: 💡 remove unused routes from hardwareRoutes

--- src/routes/hardwareRoutes.js
+++ src/routes/hardwareRoutes.js
@@ -21,44 +21,4 @@ router.post("/update/:deviceId", async (req, res) => {
     }
 });
 
-router.get("/selected-devices", async (req, res) => {
-    try {
-        const devices = await getSelectedDevices({
-            deviceIds: req.query.deviceIds,
-        });
-        res.sendResponse({ status: 1, devices });
-    } catch (err) {
-        res.sendError(err);
-    }
-});
-
-router.get("/:deviceId", async (req, res) => {
-    try {
-        const device = await getDevice({ deviceId: req.params.deviceId });
-        res.sendResponse({ status: 1, device });
-    } catch (err) {
-        res.sendError(err);
-    }
-});
-
-router.post("/change-name", async (req, res) => {
-    try {
-        const { deviceId, name } = req.body;
-        await changeDeviceName({ deviceId, name });
-        res.sendResponse({ status: 1 });
-    } catch (err) {
-        res.sendError(err);
-    }
-});
-
-router.post("/change", async (req, res) => {
-    try {
-        const { deviceIds, value, economy } = req.body;
-        await changeDeviceValue({ deviceIds, value, economy });
-        res.sendResponse({ status: 1 });
-    } catch (err) {
-        res.sendError(err);
-    }
-});
-
 module.exports = router;
