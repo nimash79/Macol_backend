08ba0f2adb18cb2879b04e8213bff6e3f8eedf32
Nima <nimash7979@gmail.com>
2025-05-08 10:33:07 +0330

    perf: ⚡️ validation for update hardware

--- src/routes/hardwareRoutes.js
+++ src/routes/hardwareRoutes.js
@@ -8,6 +8,8 @@ router.post("/update/:deviceId", async (req, res) => {
     try {
         const { deviceId } = req.params;
         const { temperature, battery } = req.body;
+        if (temperature < 15 || temperature > 30) return res.send("Invalid temperature");
+        if (battery < 0 || battery > 100) return res.send("Invalid battery");
         const device = await getAndUpdateDevice({ deviceId, temperature, battery });
         if (!device) return res.send("Device not found!");
         await addReport({ deviceId, temperature, battery });
